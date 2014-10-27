f852c91c377e3d083d7a7820155137c7afb9d83b
rachini <rachini94perera@gmail.com>
2014-10-27 21:55:45 +0530

    popup-package

--- application/views/package/manage_package_view.php
+++ application/views/package/manage_package_view.php
@@ -12,6 +12,7 @@
                 <div class="box-header">
                     <h3 class="box-title">Packages Data Table</h3>
                 </div><!-- /.box-header -->
+                <button class="btn btn-info" data-toggle="modal" data-target="#add_package_modal">Add New Package</button>
                 <div class="box-body table-responsive">
                     <table id="package_table" class="table table-bordered table-hover">
                         <thead>
@@ -44,6 +45,80 @@
     </div>
 
 </section><!-- /.content -->
+
+<div class="modal fade" id="add_package_modal" tabindex="-1" role="dialog" aria-labelledby="add_package_modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form id="add_package_form" name="add_package_form">
+                <div class="modal-header tiles green">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+
+                </div>
+                <div class="modal-body">
+
+                    <div class="row form-row">
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <label class="form-label">Package Name</label>
+                                <span style="color: red">*</span>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="input-with-icon  right">                                       
+                                <i class=""></i>
+                                <input id="package_name" class="form-control" type="text" name="package_name">                              
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row form-row">
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <label class="form-label">Max Targets</label>
+                                <span style="color: red">*</span>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="input-with-icon  right">                                       
+                                <i class=""></i>
+                                <input id="max_targets" class="form-control" type="text" name="max_targets">                              
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row form-row">
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <label class="form-label">Max Objects</label>
+                                <span style="color: red">*</span>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="input-with-icon  right">                                       
+                                <i class=""></i>
+                                <input id="max_objects" class="form-control" type="text" name="max_objects">                              
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+                <div id="add_package_msg" class="form-row"> </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                </div>
+
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
 <script type="text/javascript">
-    $('#app_parent_menu').addClass('active open');
+    $('#package_parent_menu').addClass('active open');
 </script>
