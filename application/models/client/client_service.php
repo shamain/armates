f77ac2cd2e7eef40a4a89a6387f0db7880f1c704
gayathma <gayathma3@gmail.com>
2014-10-29 09:03:24 +0530

    sdadsd

--- application/models/client/client_service.php
+++ application/models/client/client_service.php
@@ -1,6 +1,6 @@
 <?php
 
-class Clients_service extends CI_Model {
+class Client_service extends CI_Model {
     
      function __construct() {
         parent::__construct();
--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -10,3 +10,16 @@
 
     </ul>
 </li>
+
+
+<li class="treeview" id="alient_parent_menu">
+    <a href="#">
+        <i class="fa fa-laptop"></i>
+        <span>Clients</span>
+        <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+        <li><a href="<?php echo site_url(); ?>/client/client_controller/manage_clients"><i class="fa fa-angle-double-right"></i> My Apps</a></li>
+        
+    </ul>
+</li>
