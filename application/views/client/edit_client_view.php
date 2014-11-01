36317ea9228e5dde78bbcea4893befc8c2ca941d
kaumadi <bhagyakaumadi@gmail.com>
2014-11-01 15:11:39 +0530

    chznges

--- application/views/client/edit_client_view.php
+++ application/views/client/edit_client_view.php
@@ -2,84 +2,45 @@
     <h3><?php echo $heading; ?></h3>		
 </div>
 
-<div class="row-fluid">
-    <div class="span12">
-        <div class="grid simple ">
-            
-            <div class="grid-body ">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <form id="edit_client_form" name="edit_client_form">
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-primary">
+                <div class="box-header">
+                    <h3 class="box-title">Edit Client Details</h3>
+                </div>
+                <form role="form" id="edit_client_form" name=edit_client_form">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="client_fname">First Name</label>
+                            <input id="client_fname" class="form-control" type="text" name="client_fname"  value="<?php echo $client->client_fname; ?>"style="width: 50%"> 
+                        </div>
 
                           <div class="form-group">
-                                <label class="form-label">First Name</label>
-                                <span style="color: red">*</span>
+                            <label for="client_lname">Last Name</label>
+                            <input id="client_lname" class="form-control" type="text" name="client_lname"  value="<?php echo $client->client_lname; ?>"style="width: 50%"> 
+                        </div>
+                         <div class="form-group">
+                            <label for="client_password">Password</label>
+                            <input id="client_password" class="form-control" type="text" name="client_password"  value="<?php echo $client->client_password; ?>"style="width: 50%"> 
+                        </div>
 
-                                <div class="input-with-icon  right">                                       
-                                    <i class=""></i>
-                                    <input id="client_fname" class="form-control" type="text" name="client_fname" value="<?php echo $client->client_fname; ?>" style="width: 50%">                              
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="form-label">Last Name</label>
-                                <span style="color: red">*</span>
-
-                                <div class="input-with-icon  right">                                       
-                                    <i class=""></i>
-                                    <input id="client_lname" class="form-control" type="text" name="client_lname" value="<?php echo $client->client_lname; ?>" style="width: 50%">                              
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="form-label">Password</label>
-                                <span style="color: red">*</span>
+                         <div class="form-group">
+                            <label for="client_email">Email</label>
+                            <input id="client_email" class="form-control" type="text" name="client_email"  value="<?php echo $client->client_email; ?>"style="width: 50%"> 
+                        </div>
+                            <div class="form-group" >
+                            <label for="client_bday">Birth Day</label>
+                            <input id="client_bday_edit_dpicker" class="form-control"  type="text" input-append id="client_bday" name="client_bday" readonly="true"  value="<?php echo $client->client_bday; ?>"style="width: 50%">
+                        </div>
 
 
-                                <div class="input-with-icon  right">                                       
-                                    <i class=""></i>
-                                    <input id="client_password" class="form-control" type="text" name="client_password" value="<?php echo $client->client_password; ?>" style="width: 50%"> 
-                                </div>
+                         <div class="form-group">
+                            <label for="client_contact">Contact No</label>
+                            <input id="client_contact" class="form-control" type="text" name="client_contact"  value="<?php echo $client->client_contact; ?>"style="width: 50%"> 
+                        </div>
 
-
-
-
-                            </div>
-
-                            <div class="form-group">
-                                <label class="form-label">Email</label>
-                                <span style="color: red">*</span>
-
-                                <div class="input-with-icon  right">                                       
-                                    <i class=""></i>
-                                    <input id="client_email" class="form-control" type="text" name="client_email" value="<?php echo $client->client_email; ?>" style="width: 50%">                              
-                                </div>
-                            </div>
                             
-                              <div class="form-group">
-                                <label class="form-label">Birth Day</label>
-                                <span style="color: red">*</span><br>
-
-                                <div class="input-with-icon  right input-append primary date  no-padding" id="client_bday_edit_dpicker">                                       
-                                    <i class=""></i>
-
-                                    <input class="form-control" type="text" input-append id="client_bday" name="client_bday" readonly="true"  value="<?php echo $client->client_bday; ?>"style="width: 50%">
-                                    <span class="add-on">
-                                        <span class="arrow"></span>
-                                        <i class="fa fa-th"></i>
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="form-label">Contact No</label>
-                                <span style="color: red">*</span>
-
-                                <div class="input-with-icon  right">                                       
-                                    <i class=""></i>
-                                    <input id="client_contact" class="form-control" type="text" name="client_contact" value="<?php echo $client->client_contact; ?>" style="width: 50%">                              
-                                </div>
-                            </div>
 
                                <div class="form-group">
                                 <label class="form-label">Avatar</label>
@@ -132,74 +93,46 @@
 
 
                                 </script>
+                           <div class="row form-row">
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <div id="files" class="project-logo">
 
-                                <div class="row form-row">
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <div id="files" class="project-logo">
-                                                <img src="<?PHP echo base_url(); ?>uploads/clients/<?php
-                                                if ($client->client_avatar != '') {
-                                                    echo $client->client_avatar;
-                                                } else {
-                                                    echo 'avatar.jpg';
-                                                }
-                                                ?>" alt="" width="100px" height="100px"  />
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class=" right">                                       
-                                            <i class=""></i>
-
-                                            <div id="upload">
-
-                                                <input type="text" style="visibility: hidden" id="image" name="image"/>
-                                                <button class="btn btn-default btn-sm btn-small" type="button" id="browse">
-                                                    <i class="fa fa-camera"></i>
-                                                </button>
-                                                <label class="form-label">upload Image Here</label>
-                                                <input type="hidden" id="client_avatar" name="client_avatar"  value="<?php
-                                                if ($client->client_avatar != '') {
-                                                    echo $client->client_avatar;
-                                                } else {
-                                                    echo 'avatar.jpg';
-                                                }
-                                                ?>"/>
-                                            </div>
-                                            <div id="sta"><span id="status" ></span></div>
-
-
-
-                                        </div>
                                     </div>
                                 </div>
-
-                            </div> 
-
-                            
-
-
-                            <div id="edit_client_msg" class="form-row"> </div>
-
-                            <input type="hidden" id="client_id" name="client_id" value="<?php echo $client->client_id; ?>"/>
-                            <div class="modal-footer">
-
-                                <button class="btn btn-primary btn-cons" type="submit">
-                                    <i class="icon-ok"></i>
-                                    Save
-                                </button>
-                                <a href="<?php echo site_url(); ?>/client/client_controller/manage_clients" class="btn btn-white btn-cons" type="button">Cancel</a>
-
                             </div>
+                            <div class="col-md-6">
+                                <div class=" right">                                       
+                                    <i class=""></i>
 
-                        </form>
+                                    <div id="upload">
+                                        <button class="btn btn-default btn-sm btn-small" type="button" id="browse">
+                                            Upload Image Here
+                                        </button>
+                                        <input type="text" id="upload_client_image" name="upload_client_image" style="visibility: hidden" />
+                                    </div>
+                                    <div id="sta"><span id="status" ></span></div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div id="add_client_msg"> </div>
+                        </div>
+
+
                     </div>
-                </div>
+                    <div class="box-footer">
+                        <button class="btn btn-primary" type="submit">Save</button>
+                        <a href="<?php echo site_url(); ?>/client/client_controller/manage_clients" class="btn btn-white btn-cons" type="button">Cancel</a>
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>
-</div>
+</section>
 <script type="text/javascript">
     $('#client_parent_menu').addClass('active open');
     
